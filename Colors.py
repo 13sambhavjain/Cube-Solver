9ae537fbc6b74241758dcd4c3bcd0ac5b91eb8e3
13sambhavjain <13sambhavjain@gmail.com>
2025-11-06 18:50:39 +0530

    Improving Color class: formatting, representation, ans ANSI code for coloring background in terminal. Colors: Adding repr and __init__ [just for reversability] methods.

--- Colors.py
+++ Colors.py
@@ -1,5 +1,15 @@
 from enum import StrEnum, auto
 
+_COLOR_ANSI_MAP = {
+    'white':  '\033[47m', 
+    'blue':   '\033[44m',
+    'orange': '\033[48;5;208m',
+    'yellow': '\033[43m',
+    'green':  '\033[42m',
+    'red':    '\033[41m',
+}
+_RESET_CODE = '\033[0m'
+
 class Color(StrEnum):
     white = auto()
     blue = auto()
@@ -7,6 +17,32 @@ class Color(StrEnum):
     yellow = auto()
     green = auto()
     red = auto()
+
+    @classmethod
+    def get_reset_code(cls):
+        return _RESET_CODE
+
+    def __format__(self, format_spec: str):
+        """
+        Custom formatting using format specifiers:
+        - 's': returns the color name
+        - 'a': returns the ANSI background code + reset code
+        - 'i': returns the ANSI background code + initial letter + reset code
+        """
+        if format_spec == 'i':
+            # Returns background code + initial letter + RESET
+            return f"{_COLOR_ANSI_MAP[self.name]}{self.name[0].upper():^3}{_RESET_CODE}"
+        elif format_spec == 'a':
+            # Returns just the background code + two spaces + RESET
+            return f"{_COLOR_ANSI_MAP[self.name]}  {_RESET_CODE}"
+        elif format_spec == 's' or format_spec == '':
+            return str(self)
+        else:
+            return format(str(self), format_spec)
+
+    def __repr__(self):
+        # Returns a string that looks like "Color.<ColorName>"
+        return f"{self.__class__.__name__}.{self.name}"
 
 class Colors():
     """A collection of Colors with cyclic indexing and iteration."""
@@ -18,6 +54,14 @@ class Colors():
         if cls._instance is None:
             cls._instance = super(Colors, cls).__new__(cls)
         return cls._instance
+    
+    def __init__(self, *args, **kwargs):
+        """
+        Flexible initializer that accepts any arguments.
+        It doesn't actually use args/kwargs for anything in this specific setup,
+        but it makes the class definition resilient to being called with arguments.
+        """
+        pass
 
     def __iter__(self):
         """Allow iteration: for color in Colors"""
@@ -51,7 +95,7 @@ class Colors():
     
     def __repr__(self):
         """Official string representation of the Colors collection."""
-        return f'Colors(_colors={self._colors}, _instance={object.__repr__(self._instance) if self._instance else None})'
+        return f'Colors(_colors={self._colors})'
 
     def __str__(self):
         """String representation of the Colors collection."""
@@ -68,4 +112,5 @@ if __name__ == "__main__":
     # print(colors.index(Color.green))
     # print(len(colors))
     # print(colors.__repr__())
+    print(Color.white.__repr__())
     pass
